a321a76251823aed49c449c153f897e522f686bb
Robert Fancsik <frobert@inf.u-szeged.hu>
2017-10-27 20:54:39 +0200

    Fix mistake during resolving confilcts.

--- src/main/java/io/github/rerobika/rf1/domain/User.java
+++ src/main/java/io/github/rerobika/rf1/domain/User.java
@@ -3,10 +3,7 @@ package io.github.rerobika.rf1.domain;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
 
-import org.springframework.security.core.GrantedAuthority;
-
 import javax.persistence.*;
-import javax.validation.constraints.Size;
 import java.util.List;
 
 /**
@@ -42,28 +39,6 @@ public class User {
     public User() {}
 
     public User(String name, List<Album> albums, List<Message> messages, List<Post> posts) {
-    @Column(name = "email", unique=true)
-    @Email(message="{register.email.invalid}")
-    @NotBlank(message="{register.email.invalid}")
-    private String email;
-
-    @Column(length=20)
-    private String role;
-
-    @Transient
-    @Size(min=5, max=15, message="{register.password.size}")
-    private String plainPassword;
-
-    @Column(name = "password", length=60)
-    private String password;
-
-    @Transient
-    private String repeatPassword;
-
-    @OneToOne
-    private Profile profile;
-
-    public User(String name, Profile profile) {
         this.name = name;
         this.albums = albums;
         this.messages = messages;
@@ -150,43 +125,4 @@ public class User {
         this.role = role;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPlainPassword() {
-        return plainPassword;
-    }
-
-    public void setPlainPassword(String plainPassword) {
-        this.plainPassword = plainPassword;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRepeatPassword() {
-        return repeatPassword;
-    }
-
-    public void setRepeatPassword(String repeatPassword) {
-        this.repeatPassword = repeatPassword;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
 }
