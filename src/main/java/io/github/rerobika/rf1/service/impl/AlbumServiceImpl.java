4ee786069dec70e0ca09b91187fa6bf1c6f3f926
Milko96 <furi.milan@hotmail.com>
2017-11-02 23:14:16 +0100

    title
    Adding Album and Picture service

--- src/main/java/io/github/rerobika/rf1/service/impl/AlbumServiceImpl.java
+++ src/main/java/io/github/rerobika/rf1/service/impl/AlbumServiceImpl.java
@@ -2,27 +2,33 @@ package io.github.rerobika.rf1.service.impl;
 
 import io.github.rerobika.rf1.domain.Album;
 import io.github.rerobika.rf1.service.AlbumService;
+import io.github.rerobika.rf1.repository.AlbumRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+import java.util.ArrayList;
+@Service
 public class AlbumServiceImpl implements AlbumService {
+    @Autowired
+    private AlbumRepository albumRep;
+
     @Override
-    public List<Album> getAll() {
-        return null;
+    public List<Album> getAll()
+    {
+        if(albumRep.count()!=0)
+            return (List<Album>) albumRep.findAll();
+        else return  new ArrayList<Album>();
     }
 
     @Override
-    public void addAlbum(Album album) {
-
-    }
+    public void addAlbum(Album album) {albumRep.save(album);}
 
     @Override
-    public void removeAlbum(Album album) {
-
-    }
+    public void removeAlbum(Album album) {albumRep.delete(album);}
 
     @Override
     public Album getAlbum(long id) {
-        return null;
+        return albumRep.findOne(id);
     }
 }
--- src/main/java/io/github/rerobika/rf1/service/impl/PictureServiceImpl.java
+++ src/main/java/io/github/rerobika/rf1/service/impl/PictureServiceImpl.java
@@ -2,27 +2,31 @@ package io.github.rerobika.rf1.service.impl;
 
 import io.github.rerobika.rf1.domain.Picture;
 import io.github.rerobika.rf1.service.PictureService;
+import io.github.rerobika.rf1.repository.PictureRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+import java.util.ArrayList;
+@Service
 public class PictureServiceImpl implements PictureService {
+    @Autowired
+    private PictureRepository pictureRep;
+
     @Override
-    public List<Picture> getAll() {
-        return null;
+    public List<Picture> getAll()
+    {
+        if(pictureRep.count()!=0)
+            return (List<Picture>) pictureRep.findAll();
+        else return  new ArrayList<Picture>();
     }
 
     @Override
-    public void addPicture(Picture picture) {
-
-    }
+    public void addPicture(Picture picture) {pictureRep.save(picture);}
 
     @Override
-    public void removePicture(Picture picture) {
-
-    }
+    public void removePicture(Picture picture) {pictureRep.delete(picture);}
 
     @Override
-    public Picture getPicture(long id) {
-        return null;
-    }
+    public Picture getPicture(long id) {return pictureRep.findOne(id);}
 }
