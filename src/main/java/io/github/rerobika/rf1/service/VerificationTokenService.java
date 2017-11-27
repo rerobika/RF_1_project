c97ed8b230c62eb1e518103b2e739f9e5923a769
Robert Fancsik <frobert@inf.u-szeged.hu>
2017-11-27 18:58:59 +0100

    Added ref to friend feature

---
+++ src/main/java/io/github/rerobika/rf1/service/VerificationTokenService.java
@@ -0,0 +1,7 @@
+package io.github.rerobika.rf1.service;
+
+import io.github.rerobika.rf1.domain.VerificationToken;
+
+public interface VerificationTokenService {
+    VerificationToken getTokenByName(String token);
+}
